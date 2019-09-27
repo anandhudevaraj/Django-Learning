6fc7c33c8525e43d88ef3f71487fc5a16d90d4e2
Anandhu <anandhudevaraj@gmail.com>
2019-09-27 23:51:19 +0530

    added app.css, base.html, and lot more

--- my_project/my_project/boards/tests.py
+++ my_project/my_project/boards/tests.py
@@ -1,14 +1,47 @@
 from django.test import TestCase
 from django.urls import reverse, resolve
-from .views import home
+from .views import home, board_topics
+from .models import Board
 
 
 class HomeTests(TestCase):
-    def test_home_view_status_code(self):
+    def setUp(self):
+        self.board = Board.objects.create(name='Django', description="Some django board")
         url = reverse('home')
-        response = self.client.get(url)
-        self.assertEquals(response.status_code, 200)
+        self.response = self.client.get(url)
+
+    def test_home_view_status_code(self):
+        self.assertEquals(self.response.status_code, 200)
 
     def test_home_url_resolves_home_view(self):
         view = resolve('/')
         self.assertEquals(view.func, home)
+
+    def test_home_view_contains_lins_to_topics(self):
+        url = reverse('board_topics', kwargs={'pk': self.board.pk})
+        self.assertContains(self.response, 'href="{0}"'.format(url))
+
+
+class BoardTopicsTests(TestCase):
+    def setUp(self):
+        Board.objects.create(name='Django', description='Some Django Board')
+
+    def test_board_topics_view_status_code_ok(self):
+        url = reverse('board_topics', kwargs={'pk': 1})
+        response = self.client.get(url)
+        self.assertEquals(response.status_code, 200)
+
+    def test_board_topics_view_not_found_status_Code(self):
+        url = reverse('board_topics', kwargs={'pk': 99})
+        response = self.client.get(url)
+        self.assertEquals(response.status_code, 404)
+
+    def test_board_topics_url_returns_board_topics_view(self):
+        view = resolve('/boards/1/')
+        self.assertEquals(view.func, board_topics)
+
+    def test_board_topic_have_link_to_home(self):
+        board_topic_url = reverse('board_topics', kwargs={'pk': 1})
+        response = self.client.get(board_topic_url)
+        home_url = reverse('home')
+        self.assertContains(response, 'href="{0}"'.format(home_url))
--- my_project/my_project/my_project/urls.py
+++ my_project/my_project/my_project/urls.py
@@ -19,6 +19,7 @@ from django.urls import path
 from boards import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
     path('admin/', admin.site.urls),
+    path('boards/<int:pk>/', views.board_topics, name='board_topics'),
+    path('', views.home, name='home'),
 ]
